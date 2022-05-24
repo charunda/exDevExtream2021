014380a7547cb45f4e383b9d9e915b2e17372cf2
charunda <33979697+charunda@users.noreply.github.com>
2022-05-24 14:54:41 +0700

    Update FormDevExtream.tsx

--- FormDevExtream.tsx
+++ FormDevExtream.tsx
@@ -1,1 +1,35 @@
-
+import React from 'react';
+ 
+import 'devextreme/dist/css/dx.light.css';
+ 
+import Form from 'devextreme-react/form';
+ 
+class App extends React.Component {
+    employee: {
+        firstName: 'John',
+        lastName: 'Heart'
+    }
+ 
+    customizeItem(item) {
+        if (item.dataField === "FirstName" || item.dataField === "LastName") {
+            item.validationRules = [{
+                type: "required",
+                message: "The value is required"
+            }, {
+                type: "pattern",
+                pattern: "^[a-zA-Z]+$",
+                message: "The value should not contain digits"
+            }]
+        }
+    }
+ 
+    render() {
+        return (
+            <Form
+                formData={this.employee}
+                customizeItem={this.customizeItem}>
+            </Form>
+        );
+    }
+}
+export default App;
